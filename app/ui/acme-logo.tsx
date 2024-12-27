9b976c385f9be2cb13afb94d0882386c572342a4
supsokman <supsokman@gmail.com>
2024-12-27 14:45:15 +0900

    폰트추가

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -5,7 +5,7 @@ export default function AcmeLogo() {
     <div
       className={` flex flex-row items-center leading-none text-white`}
     >
-      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
+
       <p className="text-[44px]">Acme</p>
     </div>
   );
